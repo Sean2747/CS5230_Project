c1e804bb0985b5e21e982dbca188d0257db3feb0
Sean2747 <shanjiangca@gmail.com>
2025-11-20 18:49:35 -0800

    launch file added

---
+++ src/scout_controller/launch/gazebo_with_bridge.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Use $HOME/ros2_ws/worlds/enclosed.sdf
+    world_path = PathJoinSubstitution([
+        EnvironmentVariable('HOME'),
+        'ros2_ws',
+        'worlds',
+        'enclosed.sdf'
+    ])
+
+    # Set LIBGL_ALWAYS_SOFTWARE=1 for software rendering
+    set_libgl_env = SetEnvironmentVariable(
+        name='LIBGL_ALWAYS_SOFTWARE',
+        value='1'
+    )
+
+    # Launch Gazebo world
+    gazebo = ExecuteProcess(
+        cmd=[
+            'ign', 'gazebo',
+            '-r',
+            '-v', '4',
+            world_path
+        ],
+        output='screen'
+    )
+
+    # Start ros_gz_bridge parameter_bridge for your topics
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/model/vehicle/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+            '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock',
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        set_libgl_env,
+        gazebo,
+        bridge,
+    ])
--- src/scout_controller/scout_controller/wander.py
+++ src/scout_controller/scout_controller/wander.py
@@ -16,7 +16,7 @@ class WanderNode(Node):
     def send_velocity_command(self):
         msg = Twist()
         if self.turning == True:
-            msg.linear.x = 0.0
+            msg.linear.x = 0.2
             msg.angular.z = 0.5
         else:
             msg.linear.x = 1.5
@@ -26,10 +26,8 @@ class WanderNode(Node):
     def lidar_callback(self, msg: LaserScan):
         i = 319
         window = msg.ranges[max(0, i-15):min(len(msg.ranges), i+16)]
-
         vals = [r for r in window if math.isfinite(r) and r > msg.range_min]
         front_distance = min(vals) if vals else msg.range_max
-        #front_distance = min(min(msg.ranges[0:20] + msg.ranges[-20:]), 10.0)
 
         if front_distance < 4.0:
             self.turning = True
