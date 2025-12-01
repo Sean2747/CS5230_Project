57d33bebc0c018ea9cfd98c651a59583868d8e09
Sean2747 <shanjiangca@gmail.com>
2025-12-01 15:37:53 -0800

    Updated mapper params, robot core xacro, and new map files

--- src/scout_controller/launch/gazebo_with_bridge.launch.py
+++ src/scout_controller/launch/gazebo_with_bridge.launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
@@ -13,6 +15,8 @@ def generate_launch_description():
         'worlds',
         'enclosed.sdf'
     ])
+
+    #world_path = '/home/shanj/ros2_ws/src/scout_controller/worlds/enclosed.sdf'
 
     # Set LIBGL_ALWAYS_SOFTWARE=1 for software rendering
     set_libgl_env = SetEnvironmentVariable(
@@ -37,6 +41,7 @@ def generate_launch_description():
         executable='parameter_bridge',
         arguments=[
             '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/model/vehicle/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
             '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
             '/model/vehicle/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
             '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock',
